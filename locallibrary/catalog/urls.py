a12ffb03c00fae015ccbaed333db54bf75f8fdf3
i-lac <241275512+i-lac@users.noreply.github.com>
2026-08-24 22:37:20 +1000

    Complete tutorial 6

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -4,6 +4,10 @@ from . import views
 # home page
 urlpatterns = [
     path('', views.index, name='index'),
+    path('books/', views.BookListView.as_view(), name='books'),
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
+    path('authors/', views.AuthorListView.as_view(), name='authors'),
+    path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
 ]
 """
 From https://developer.mozilla.org/en-US/docs/Learn_web_development/Extensions/Server-side/Django/Home_page:
