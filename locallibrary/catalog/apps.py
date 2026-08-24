5b8fe9efd2c46b09475f1331afe479586e22939a
i-lac <241275512+i-lac@users.noreply.github.com>
2026-08-24 14:24:43 +1000

    Add explanatory comments to apps.py

--- locallibrary/catalog/apps.py
+++ locallibrary/catalog/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
 
-
+# This class is used to configure the catalog application. 
+# This was automatically generated when the application was created using the "startapp" command.
 class CatalogConfig(AppConfig):
+    # the default_auto_field attribute specifies the type of primary key to use for models in this application.
     default_auto_field = 'django.db.models.BigAutoField'
+    # the name attribute specifies the full Python path to the application.
     name = 'catalog'
