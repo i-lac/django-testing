2de0cd13bdc75c97eb4cbbbeb6d833a51db1e06d
i-lac <241275512+i-lac@users.noreply.github.com>
2026-08-24 19:12:16 +1000

    Complete tutorial 5

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Book, Author, BookInstance, Genre
+
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    # Available books (status = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+
+    # The 'all()' is implied by default.
+    num_authors = Author.objects.count()
+
+    # counts for genres and books that contain a particular word
+    num_fiction_genres = Genre.objects.filter(name__contains='Fiction').count()
+    num_ninth_books = Book.objects.filter(title__contains='Ninth').count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_fiction_genres': num_fiction_genres,
+        'num_ninth_books': num_ninth_books,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
