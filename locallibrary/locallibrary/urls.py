5ff43cb6bb55eb88f9badfca1ea31cf45955228c
i-lac <241275512+i-lac@users.noreply.github.com>
2026-08-25 11:16:55 +1000

    Remove catalog redirect

--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -29,10 +29,10 @@ urlpatterns += [
 ]
 
 # Add URL maps to redirect the base URL to our application
-from django.views.generic import RedirectView
-urlpatterns += [
-    path('', RedirectView.as_view(url='catalog/', permanent=True)),
-]
+# from django.views.generic import RedirectView
+# urlpatterns += [
+#     path('', RedirectView.as_view(url='catalog/', permanent=True)),
+# ]
 
 # Use static() to add URL mapping to serve static files during development (only)
 from django.conf import settings
