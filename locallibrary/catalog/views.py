7e5d9cf0797335c22f78e246b911142e3bcc1ddd
i-lac <241275512+i-lac@users.noreply.github.com>
2026-09-16 13:14:41 +1000

    Add session handling view count

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -16,6 +16,14 @@ def index(request):
     # The 'all()' is implied by default.
     num_authors = Author.objects.count()
 
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    print("num_visits" + str(num_visits))
+    num_visits += 1
+    print("num_visits after incrementing:" + str(num_visits))
+    request.session['num_visits'] = num_visits
+    print("num_visits in session:" + str(request.session['num_visits']))
+
     # counts for genres and books that contain a particular word
     num_fiction_genres = Genre.objects.filter(name__contains='Fiction').count()
     num_ninth_books = Book.objects.filter(title__contains='Ninth').count()
@@ -27,6 +35,7 @@ def index(request):
         'num_authors': num_authors,
         'num_fiction_genres': num_fiction_genres,
         'num_ninth_books': num_ninth_books,
+        'num_visits': num_visits,
     }
 
     # Render the HTML template index.html with the data in the context variable
