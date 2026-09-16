45374e8fec4312f5afc9dc39e14ae118925075b0
i-lac <241275512+i-lac@users.noreply.github.com>
2026-09-16 17:25:09 +1000

    Let due dates be renewed

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -31,3 +31,7 @@ urlpatterns += [
 urlpatterns += [
     path('allborrowed/', views.LoanedBooksAllListView.as_view(), name='all-borrowed'),
 ]
+
+urlpatterns += [
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
+]
